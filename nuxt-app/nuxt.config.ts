f9391184b40fac92708469047aa2d38932fc46ea
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2022-12-26 15:32:56 +0000

    Add basic metadata to head

--- nuxt-app/nuxt.config.ts
+++ nuxt-app/nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: "nuxt-3-starter",
+      charset: "utf-16",
+      viewport: "width=device-width, initial-scale=1",
+      meta: [{ name: "description", content: "Starter template for Nuxt 3." }],
+    },
+  },
   modules: [
     "@nuxt/content",
     "@nuxt/image-edge",
