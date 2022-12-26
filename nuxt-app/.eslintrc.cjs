7a10a074978743de40c70b88cfe3c7328dace884
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2022-12-26 15:25:42 +0000

    Use prettier with eslint

--- nuxt-app/.eslintrc.cjs
+++ nuxt-app/.eslintrc.cjs
@@ -1,4 +1,16 @@
 module.exports = {
   root: true,
-  extends: ["@nuxtjs/eslint-config-typescript"],
+  env: {
+    browser: true,
+    "jest/globals": true,
+    node: true,
+  },
+  parserOptions: {
+    parser: "@babel/eslint-parser",
+    requireConfigFile: false,
+  },
+  extends: ["@nuxtjs/eslint-config-typescript", "plugin:prettier/recommended"],
+  plugins: ["jest"],
+  // add your custom rules here
+  rules: {},
 };
