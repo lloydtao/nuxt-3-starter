c6bbc63492732d440227b946b57d2abf923d3944
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2022-12-26 12:57:57 +0000

    Add note on Nuxt Image module to home page

--- nuxt-app/components/StarterBody.vue
+++ nuxt-app/components/StarterBody.vue
@@ -49,6 +49,22 @@
           <span class="font-semibold">Content</span> for a file-based CMS
         </p>
       </li>
+      <li class="flex items-center">
+        <svg
+          class="h-6 w-6 flex-none fill-sky-100 stroke-sky-500 stroke-2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <circle cx="12" cy="12" r="11"></circle>
+          <path
+            d="m8 13 2.165 2.165a1 1 0 0 0 1.521-.126L16 9"
+            fill="none"
+          ></path>
+        </svg>
+        <p class="ml-4 text-slate-800">
+          <span class="font-semibold">Image</span> for image optimisation
+        </p>
+      </li>
     </ul>
   </div>
 </template>
