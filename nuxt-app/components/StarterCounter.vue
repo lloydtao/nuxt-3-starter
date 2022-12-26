8a40efdf416120e4f191510cf467a02bebea95e2
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2022-12-26 12:37:55 +0000

    Add counter to home page

---
+++ nuxt-app/components/StarterCounter.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div class="text-base font-semibold leading-7">
+      <p class="text-gray-900 text-center">Counter: {{ count }}</p>
+      <button
+        type="button"
+        class="mt-1 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+        @click="increment"
+      >
+        Increment
+      </button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { storeToRefs } from "pinia";
+import { useCounterStore } from "@/stores/counter";
+
+export default defineComponent({
+  setup() {
+    const store = useCounterStore();
+    const { count, doubleCount } = storeToRefs(store);
+    const { increment } = store;
+
+    return {
+      count,
+      doubleCount,
+      increment,
+    };
+  },
+});
+</script>
--- nuxt-app/pages/index.vue
+++ nuxt-app/pages/index.vue
@@ -2,5 +2,6 @@
   <div class="mx-auto max-w-md">
     <StarterHeading class="pb-5" />
     <StarterBody class="pb-5" />
+    <StarterCounter class="pt-5 border-t divide-slate-200" />
   </div>
 </template>
