2d6d5bcddc1aeceac0fc8f2ef5e4db0085407b2c
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2022-12-26 15:36:07 +0000

    Add note on linting modules to home page

--- nuxt-app/components/StarterBody.vue
+++ nuxt-app/components/StarterBody.vue
@@ -53,6 +53,20 @@
           <span class="font-semibold">Nuxt Image</span> for image optimisation
         </p>
       </li>
+      <li class="flex items-center">
+        <svg
+          class="h-6 w-6 flex-none fill-sky-100 stroke-sky-500 stroke-2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <circle cx="12" cy="12" r="11" />
+          <path d="m8 13 2.165 2.165a1 1 0 0 0 1.521-.126L16 9" fill="none" />
+        </svg>
+        <p class="ml-4 text-slate-800">
+          <span class="font-semibold">ESLint</span> and
+          <span class="font-semibold">Prettier</span> for linting
+        </p>
+      </li>
     </ul>
   </div>
 </template>
