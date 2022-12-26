b2f3bc93b2c6da36ffcb85bcb47ef292ff5b8f93
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2022-12-26 12:32:40 +0000

    Create basic home page using components

---
+++ nuxt-app/components/StarterHeading.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <img src="/icon.png" class="w-16" alt="Nuxt logo" />
+    <h1 class="font-medium text-3xl text-slate-900">nuxt-3-starter</h1>
+  </div>
+</template>
--- nuxt-app/layouts/default.vue
+++ nuxt-app/layouts/default.vue
@@ -1,5 +1,20 @@
 <template>
-  <div>
-    <slot />
+  <div
+    class="relative flex min-h-screen flex-col justify-center overflow-hidden bg-gray-50 py-6 sm:py-12"
+  >
+    <img
+      src="/img/beams.jpg"
+      alt=""
+      class="absolute top-1/2 left-1/2 max-w-none -translate-x-1/2 -translate-y-1/2"
+      width="1308"
+    />
+    <div
+      class="absolute inset-0 bg-[url(/img/grid.svg)] bg-center [mask-image:linear-gradient(180deg,white,rgba(255,255,255,0))]"
+    ></div>
+    <div
+      class="relative bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10"
+    >
+      <slot />
+    </div>
   </div>
 </template>
