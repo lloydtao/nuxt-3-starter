d5ff0cc3038ce7689e6b0b9b2bb6f20f97c4b582
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2022-12-27 12:40:41 +0000

    Add notes on testing to readme and home page

--- nuxt-app/components/StarterBody.vue
+++ nuxt-app/components/StarterBody.vue
@@ -67,6 +67,19 @@
           <span class="font-semibold">Prettier</span> for linting
         </p>
       </li>
+      <li class="flex items-center">
+        <svg
+          class="h-6 w-6 flex-none fill-sky-100 stroke-sky-500 stroke-2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <circle cx="12" cy="12" r="11" />
+          <path d="m8 13 2.165 2.165a1 1 0 0 0 1.521-.126L16 9" fill="none" />
+        </svg>
+        <p class="ml-4 text-slate-800">
+          <span class="font-semibold">Vitest</span> for testing
+        </p>
+      </li>
     </ul>
   </div>
 </template>
