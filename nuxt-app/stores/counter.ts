58ee3f803c657409545400ee91bb6ccc2d6fbf46
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2022-12-30 13:10:09 +0000

    Refactor store to use best practises

--- nuxt-app/stores/counter.ts
+++ nuxt-app/stores/counter.ts
@@ -5,7 +5,7 @@ export const useCounterStore = defineStore("counter", {
     count: 0,
   }),
   getters: {
-    doubleCount: (state) => state.count * 2,
+    getCount: (state) => state.count,
   },
   actions: {
     increment() {
